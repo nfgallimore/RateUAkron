20600037927a39626a6daa76415796faee163349
root <root@ip-172-31-87-212.ec2.internal>
2017-11-20 00:33:20 +0000

    Modifed sql script attribute values

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -7,12 +7,12 @@ CREATE TABLE Courses(
 	Description varchar(1000) NOT NULL,
 	Title varchar(100) NOT NULL,
 	Career varchar(55) NOT NULL,
-	Section varchar(3) NOT NULL,
+	Section varchar(25) NOT NULL,
 	Days varchar(100) NOT NULL,
-	Credit decimal(2,1) NOT NULL,
-	Start_Time varchar(8) NOT NULL,
-	Course varchar(8) NOT NULL,
-	End_Time varchar(8) NOT NULL,
+	Credit varchar(25) NOT NULL,
+	Start_Time varchar(25) NOT NULL,
+	Course varchar(25) NOT NULL,
+	End_Time varchar(25) NOT NULL,
 	Location varchar(100) NOT NULL,
 	Department varchar(100) NOT NULL,
 	Instructor_Email varchar(100) NOT NULL,
@@ -23,25 +23,25 @@ CREATE TABLE Courses(
 	PRIMARY KEY (Id)
 );
 
-CREATE TABLE Students(
-	Id int NOT NULL,
-	Fname varchar(32) NOT NULL,
-	Lname varchar(32) NOT NULL,
-	Email varchar(200) NOT NULL,
-	Password varchar(32) NOT NULL,
+CREATE TABLE Users(
+	Id int(11) NOT NULL,
+	Username varchar(50)
+	Password varchar(255) NOT NULL,
+	Created_At DATETIME DEFAULT CURRENT_TIMESTAMP
 	PRIMARY KEY (Id)
 );
 
-CREATE TABLE Evaluation(
+CREATE TABLE Evaluations(
 	Id int NOT NULL,
 	CourseID varchar(17) NOT NULL,
-	StudentID int NOT NULL,
+	UserID int(11) NOT NULL,
 	Recommended decimal(2,1) NOT NULL,
 	TimeSpent decimal(3,1) NOT NULL,
 	Reason char NOT NULL,
 	Grade varchar(2) NOT NULL,
 	GPA decimal(3,2) NOT NULL,
+	Created_At DATETIME DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (Id),
 	FOREIGN KEY(CourseID) REFERENCES Courses(Id),
-	FOREIGN KEY(StudentID) REFERENCES Students(Id)
+	FOREIGN KEY(UserID) REFERENCES Users(Id)
 );
