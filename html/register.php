cf9c6bbe1a644f0e7592a8e54292394dd8984197
Nicholas Gallimore <nick@virtualtheologies.com>
2017-12-03 13:08:06 -0500

    committing

--- html/register.php
+++ html/register.php
@@ -1,55 +1,41 @@
 <?php
-// Include config file
 require_once '../includes/config.php';
-
-// Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Validate username
-        if(empty(trim($_POST["username"]))){
-	        $username_err = "Please enter a username.";
-		    } else{
-		            // Prepare a select statement
-			            $sql = "SELECT id FROM users WHERE username = ?";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-	            // Bind variables to the prepared statement as parameters
-		                mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            // Set parameters
-	                $param_username = trim($_POST["username"]);
-
-            // Attempt to execute the prepared statement
-	                if(mysqli_stmt_execute($stmt)){
-			                /* store result */
-					                mysqli_stmt_store_result($stmt);
-
-                if(mysqli_stmt_num_rows($stmt) == 1){
-		                    $username_err = "This username is already taken.";
-				                    } else{
-						                        $username = trim($_POST["username"]);
-									                }
-											            } else{
-												                    echo "Oops! Something went wrong. Please try again later.";
-														                }
-																        }
-
-        // Close statement
-	        mysqli_stmt_close($stmt);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter a username.";
+    } 
+    else {
+	    $sql = "SELECT id FROM users WHERE username = ?";
+		if ($stmt = mysqli_prepare($link, $sql)) {
+		    mysqli_stmt_bind_param($stmt, "s", $param_username);
+		    $param_username = trim($_POST["username"]);
+		    if (mysqli_stmt_execute($stmt)) {
+			    mysqli_stmt_store_result($stmt);
+				if (mysqli_stmt_num_rows($stmt) == 1) {
+			        $username_err = "This username is already taken.";
+			    }
+			    else {
+			    	$username = trim($_POST["username"]);
+				}
+			}
+		}
+		else {
+			echo "Oops! Something went wrong. Please try again later.";
+		}
+	}
+	// Close statement
+    mysqli_stmt_close($stmt);
 		    }
-
     // Validate password
-        if(empty(trim($_POST['password']))){
-	        $password_err = "Please enter a password.";
-		    } elseif(strlen(trim($_POST['password'])) < 6){
-		            $password_err = "Password must have atleast 6 characters.";
-			        } else{
-				        $password = trim($_POST['password']);
-					    }
+    if(empty(trim($_POST['password']))){
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST['password'])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else {
+		$password = trim($_POST['password']);
+    }
 
     // Validate confirm password
         if(empty(trim($_POST["confirm_password"]))){
