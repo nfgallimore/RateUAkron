732e33b6cd39486e6ce482fbc771654d44e20222
Nicholas Gallimore <nick@virtualtheologies.com>
2017-12-03 13:41:20 -0500

    committing

--- html/welcome.php
+++ html/welcome.php
@@ -1,34 +1,33 @@
-<?php
-	session_start();
+<?php session_start();
 
-	if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
-	  	header("location: login.php");
-		exit;
-	}
-	require_once '../includes/config.php';
-	$sql = "SELECT Id, Title, Description, Instructor FROM Coursestest";
-	$courses = [];
-	if($result = mysqli_query($link, $sql)) {
-	    if(mysqli_num_rows($result) > 0) {
-	        while($row = mysqli_fetch_array($result)) {
-	            $courses[] = [
-		            'Id' => $row['Id'],
-	                'Title' => $row['Title'],
-	                'Description' => $row['Description'],
-		            'Instructor' => $row['Instructor']
-	            ];
-	        }
-		    mysqli_free_result($result);
-	    } 
-	    else {
-			echo "No records matching your query were found.";
-	    }
-	} 
-	else {
-		echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-	}
+if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+	header("location: login.php");
+	exit;
+}
+require_once '../includes/config.php';
+$sql = "SELECT Id, Title, Description, Instructor FROM Coursestest";
+$courses = [];
+if($result = mysqli_query($link, $sql)) {
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_array($result)) {
+            $courses[] = [
+	            'Id' => $row['Id'],
+                'Title' => $row['Title'],
+                'Description' => $row['Description'],
+	            'Instructor' => $row['Instructor']
+            ];
+        }
+	    mysqli_free_result($result);
+    } 
+    else {
+		echo "No records matching your query were found.";
+    }
+} 
+else {
+	echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 
-	mysqli_close($link);
+mysqli_close($link);
 ?>
 
 <!DOCTYPE html>
@@ -39,10 +38,10 @@
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.0/bootstrap-table.min.css" />
-    <link rel="stylesheet" href="../css/styles.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.0/bootstrap-table.min.js" />
-    <script src="../js/search.js" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.0/bootstrap-table.min.js"></script>
+    <script src="js/search.js"></script>
 </head>
 <body>
 	<div class="page-header">
