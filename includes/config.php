7c66b195c8d3773ad6cce2dd8abcd0a29dfe737f
Nicholas Gallimore <nick@virtualtheologies.com>
2017-12-03 22:32:01 -0500

    updated config for chromephp

--- includes/config.php
+++ includes/config.php
@@ -1,32 +1,23 @@
+<?php
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'wecandowhatever');
+define('DB_NAME', 'myproject');
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
-    <?php
+include 'ChromePhp.php';
+ChromePhp::log('This is just a log message');
+ChromePhp::warn("This is a warning message " ) ;
+ChromePhp::error("This is an error message" ) ;
+ChromePhp::log($_SERVER);
+ 
+foreach ($_SERVER as $key => $value) {
+    ChromePhp::log($key, $value);
+}
 
-    /* Database credentials. Assuming you are running MySQL
-
-    server with default setting (user 'root' with no password) */
-
-    define('DB_SERVER', 'localhost:3306');
-
-    define('DB_USERNAME', 'root');
-
-    define('DB_PASSWORD', 'wecandowhatever');
-
-    define('DB_NAME', 'myproject');
-
-    /* Attempt to connect to MySQL database */
-
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-     
-
-    // Check connection
-
-    if($link === false){
-
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-
-    }
-
-    ?>
+?>
 
