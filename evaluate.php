15146e8eabdec3990af0f81634ea8d0306cd4697
nfgallimore <nfgallimore@gmail.com>
2018-12-04 23:59:13 -0500

    Added comment box

--- evaluate.php
+++ evaluate.php
@@ -60,10 +60,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css" />
     <link rel="stylesheet" href="../css/styles.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <style type="text/css">
-		body{ font: 14px sans-serif; }
-		.wrapper{ width: 350px; padding: 20px; text-align: left;}
-	</style>
 </head>
 <body>
 	<div class="wrapper">
@@ -72,40 +68,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			<p>Please enter your responses below.</p>
 		</div>
 		<div class="nav-bar">
-			<center>
-				<a href="../welcome.php" class="btn btn-info">Home</a>
-			</center>
+			<a href="../welcome.php" class="btn btn-info">Home</a>
 		</div>
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . "?id=" . $courseid; ?>" method="post">
-			<div class="form-group <?php echo (!empty($recommended_err)) ? 'has-error' : ''; ?>">
-				<label>Recommended:<sup>*</sup></label>
-				<input type="text" name="recommended" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $recommended; ?>">
-				<span class="help-block"><?php echo $recommended_err; ?></span>
+		<div class="left input-container">
+			<form id="evalform" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . "?id=" . $courseid; ?>" method="post">
+				<div class="form-group <?php echo (!empty($recommended_err)) ? 'has-error' : ''; ?>">
+					<label>Recommended:<sup>*</sup></label>
+					<input type="text" name="recommended" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $recommended; ?>">
+					<span class="help-block"><?php echo $recommended_err; ?></span>
+				</div>
+				<div class="form-group <?php echo (!empty($timespent_err)) ? 'has-error' : ''; ?>">
+					<label>Hours spent per week:<sup>*</sup></label>
+					<input type="text" name="timespent" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $timespent; ?>">
+					<span class="help-block"><?php echo $timespent_err; ?></span>
+				</div>
+				<div class="form-group <?php echo (!empty($reason_err)) ? 'has-error' : ''; ?>">
+					<label>Reason for taking course:<sup>*</sup></label>
+					<input type="text" name="reason" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $reason; ?>">
+					<span class="help-block"><?php echo $reason_err; ?></span>
+				</div>
+				<div class="form-group <?php echo (!empty($grade_err)) ? 'has-error' : ''; ?>">
+					<label>Grade received:<sup>*</sup></label>
+					<input type="text" name="grade" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $grade; ?>">
+					<span class="help-block"><?php echo $grade_err; ?></span>
+				</div>
+				<div class="form-group <?php echo (!empty($gpa_err)) ? 'has-error' : ''; ?>">
+					<label>Current GPA:<sup>*</sup></label>
+					<input type="text" name="gpa" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $gpa; ?>">
+					<span class="help-block"><?php echo $gpa_err; ?></span>
+				</div>
+				<div class="form-group">
+					<input type="submit" class="btn btn-primary" value="Submit">
+					<input type="reset" class="btn btn-default" value="Reset">
+				</div>
+			</form>
+		</div>
+			<div class="left rt-textarea">
+				<textarea cols="70" rows="20" form="evalform" placeholder="Please enter a comment.*"></textarea>
 			</div>
-			<div class="form-group <?php echo (!empty($timespent_err)) ? 'has-error' : ''; ?>">
-				<label>Hours spent per week:<sup>*</sup></label>
-				<input type="text" name="timespent" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $timespent; ?>">
-				<span class="help-block"><?php echo $timespent_err; ?></span>
-			</div>
-			<div class="form-group <?php echo (!empty($reason_err)) ? 'has-error' : ''; ?>">
-				<label>Reason for taking course:<sup>*</sup></label>
-				<input type="text" name="reason" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $reason; ?>">
-				<span class="help-block"><?php echo $reason_err; ?></span>
-			</div>
-			<div class="form-group <?php echo (!empty($grade_err)) ? 'has-error' : ''; ?>">
-				<label>Grade received:<sup>*</sup></label>
-				<input type="text" name="grade" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $grade; ?>">
-				<span class="help-block"><?php echo $grade_err; ?></span>
-			</div>
-			<div class="form-group <?php echo (!empty($gpa_err)) ? 'has-error' : ''; ?>">
-				<label>Current GPA:<sup>*</sup></label>
-				<input type="text" name="gpa" class="form-control bfh-number" data-min="1" data-max="10" value="<?php echo $gpa; ?>">
-				<span class="help-block"><?php echo $gpa_err; ?></span>
-			</div>
-			<div class="form-group">
-				<input type="submit" class="btn btn-primary" value="Submit">
-				<input type="reset" class="btn btn-default" value="Reset">
-			</div>
-		</form>
 	</div>
 </body>
