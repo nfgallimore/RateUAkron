ece5e194d5355cf04dcf282d8a85b0988f042e0a
nfgallimore <nfgallimore@gmail.com>
2018-12-07 19:21:08 -0500

    Add implement

--- includes/config.php
+++ includes/config.php
@@ -24,6 +24,10 @@ foreach ($_SERVER as $key => $value) {
 
 require_once 'Mobile_Detect.php'
 
-
+$detect = new Mobile_Detect();
+$mobile = false;
+if ($detect->isMobile()) {
+	$mobile = true;
+}
 
 ?>
